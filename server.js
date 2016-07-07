f66b29dde82596057d0f5377a0bc17685ecf9d26
Christie R <creindle@hawaii.edu>
2016-07-06 22:00:13 -1000

    Booger is now an element :)

--- server.js
+++ server.js
@@ -13,57 +13,6 @@ var dataInput = "";
 var outputFile = "";
 var dataObjects = "";
 
-// var hydrogenHTML = fs.readFile('./public/hydrogen.html', function(err, data) {
-//   hydrogenHTMLString = data.toString();
-// });
-
-// var indexHTML = fs.readFile('./public/index.html', function(err, data) {
-//   indexHTMLString = data.toString();
-// });
-
-// var errorHTML = fs.readFile('./public/404.html', function(err, data) {
-//   errorHTMLString = data.toString();
-// });
-
-// var stylesCSS = fs.readFile('./css/styles.css', function(err, data) {
-//   //stylesCSSString = data.toString();
-// });
-//console.log("Sanity check");
-// var postData = querystring.stringify({
-//   'msg' : 'Hi'
-// });
-
-var options = {
-  //hostname: "./public/helium.html",
-  port: CONFIG.PORT,
-  path: "./",
-  method: 'POST'
-  // headers: {
-  //   'Content-Type': 'application/x-www-form-urlencoded',
-  //   'Content-Length': Buffer.byteLength(postData)
-  // }
-};
-
-var req = http.request(options, function (res) {
-
-  console.log(res);
-  // console.log("STATUS: " + res.statusCode);
-  res.setEncoding('utf8');
-  res.on('data', function(data){
-    console.log("Body" + data);
-  });
-  res.on('end', function() {
-    console.log('No more data');
-  });
-});
-
-req.on('error', function(e) {
-  console.log(e);
-});
-
-//req.write(postData);
-req.end();
-
 var server = http.createServer(function(request, response) {
   console.log("\nSanity check");
 
