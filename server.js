badcb48529954c0c892568fd1042de2227a40cfe
Christie R <creindle@hawaii.edu>
2016-07-07 21:42:23 -1000

    Need to fix code :(

--- server.js
+++ server.js
@@ -5,15 +5,16 @@ var querystring = require('querystring');
 
 var htmlToSend = "";
 var dataInput = "";
-var outputFile = "";
+//var outputFile = "";
 var dataObjects = "";
 //var getFile = "";
+var time = new Date();
 
 var server = http.createServer(function(request, response) {
   console.log("\nSanity check");
 
   console.log(request.method);
-  console.log(request.headers);
+  //console.log(request.headers);
   //console.log(req.data);
   //console.log(res.output);
 
@@ -28,7 +29,11 @@ var server = http.createServer(function(request, response) {
     //POST Method
 
     if (request.method === "POST") {
-      outputFile = dataObjects.elementName + '.html';
+      var date = "Date: " + time.toUTCString() + '\n';
+      var postYES = "HTTP/1.0 200 OK\n" + date + "Content-Type: application/json\n" + 'Content-Body: { "success" : true }\n';
+      console.log('\n*****POST*****');
+      var outputFile = dataObjects.elementName + '.html';
+
       console.log('Creating the file.');
 
       htmlToSend += "<!DOCTYPE html>\n";
@@ -48,34 +53,50 @@ var server = http.createServer(function(request, response) {
       htmlToSend += "</html>";
 
       fs.writeFile('./public/' + outputFile, htmlToSend, 'utf8', function (err) {
+        console.log("***Entering fs.writeFile***\n");
+        console.log(request.method + " " + request.url + " " + postYES);
         if (err) throw err;
         console.log('File Successfully written');
       });
     }
 
     if(request.method === "GET") {
+      console.log('\n*****GET*****');
       var getFile = "";
-      console.log('Getting the header');
 
       fs.access('./public/' + request.url, 6, function(err) {
+        console.log("***Entering fs.access***\n");
         if (err) {
           console.log(request.url + " does not exist");
-          console.log(err);
-          getFile = 404 + ".html";
+          //console.log(err);
+          //return getFile;
+          //console.log(getFile);
+          getFile = "/" + 404 + ".html";
+          console.log("*End of fs.access*\n");
+        }else {
+          getFile = request.url;
           console.log(getFile);
+          console.log("*End of fs.access*\n");
         }
-        getFile = request.url;
-        console.log(getFile);
 
         fs.readFile('./public' + getFile, function (err, data) {
+          console.log("***Entering fs.readFile***\n");
           if (err) {
-            console.log(err);
-            console.log('./public' + getFile);
+            //console.log(err);
+            //console.log('./public' + getFile);
+            getFile = 404 + ".html";
             console.log(getFile + " does not exist" );
-            return;
+
+            //getFile = 404 + ".html";
+            //console.log(getFile);
+            console.log(data);
+            //fs.readFile();
+            console.log("*End of fs.readFile*\n");
+          }else{
+            dataInput = data.toString();
+            console.log(dataInput);
+            console.log("*End of fs.readFile*\n");
           }
-          dataInput = data.toString();
-          console.log(dataInput);
         });
       });
       //console.log(Object.getOwnPropertyNames(request));
@@ -84,6 +105,35 @@ var server = http.createServer(function(request, response) {
       //console.log(response.output);
       //getFile = dataObjects.elementName;
       //console.log(getFile);
+    }
+
+    if(request.method === "PUT") {
+      //var rewriteFile = "";
+      console.log('\n*****PUT*****');
+      console.log(Object.getOwnPropertyNames(request));
+      console.log(Object.getOwnPropertyNames(response));
+      console.log(dataObjects.elementName);
+      console.log(request.url);
+
+      // fs.access('./public/' + request.url, 6, function(err) {
+      //   console.log("***Entering fs.access***\n");
+      //   if (err) {
+      //     console.log(err);
+      //     console.log(request.url + " does not exist");
+      //     rewriteFile = "/" + 404 + ".html";
+      //     console.log("*End of fs.access*\n");
+      //   }else {
+      //     rewriteFile = request.url;
+      //     console.log(rewriteFile);
+      //     console.log("*End of fs.access*\n");
+      //   }
+
+      //   fs.writeFile('./public' + rewriteFile, htmlToSend, 'utf8', function (err) {
+      //     console.log("***Entering fs.writeFile***\n");
+      //     if (err) throw err;
+      //     console.log('File Successfully written');
+      //   });
+      // });
     }
   });
 });
