26f1a6783d97fd180abd926c86b19bdc29590e56
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-24 09:10:27 -1000

    check in

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
-const qs = require('querystring');
+
 
 const server = http.createServer((request, response) => {
   let method = request.method;
   let url = request.url;
 
-  if (request.url.indexOf('public') < 1) { //Add public to url
+  if (url.indexOf('public') < 1) { //Add public to url
     url = 'public' + request.url;
   }
 
@@ -15,51 +15,24 @@ const server = http.createServer((request, response) => {
   console.log('Method: ', method);
 
   switch (url) {
-    case ('public/index.html'):
-      fs.readFile('./public/index.html', 'utf-8', (err, data) => {
-        if (err) {
-          fs.readFile('./public/404.html', 'utf-8', (err, data) => {
-            console.log('!!!!!!!', data)
-            response.writeHead(404, { 'content-type': 'text/html' });
-            response.write(data);
-            response.end();
-          })
-        } else {
-          response.writeHead(200, { 'content-type': 'text/html' });
-          response.write(data);
-          response.end();
-        }
-      });
-      break;
-    case ('public/css/styles.css'):
-      fs.readFile('./public/css/styles.css', 'utf-8', (err, data) => {
-        response.setHeader('Content-Type', 'text/css')
-        response.writeHead(200, 'OK');
-        response.write(data);
+    case 'public/':
+      fs.readFile('public/index.html', (err, data) => {
+        let data2 = data.toString();
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.write(data2)
         response.end();
       });
       break;
-    case ('public/hydrogen.html'):
-      fs.readFile('./public/hydrogen.html', 'utf-8', (err, data) => {
-        response.writeHead(200, { 'content-type': 'text/html' });
-        response.write(data);
+    case `public${request.url}`:
+      fs.readFile(url, (err, data) => {
+        response.writeHead(200, { 'Content-Type': 'text/html' })
+        response.write(data)
         response.end();
-      });
+      })
       break;
-    case ('public/helium.html'):
-      fs.readFile('./public/helium.html', 'utf-8', (err, data) => {
-        response.writeHead(200, { 'content-type': 'text/html' });
-        response.write(data);
-        response.end();
-      });
-      break;
-    case (`/public/${req.url}`):
-      fs.readFile(`./public/${request.url}`, 'utf-8', (err, data) => {
-        response.writeHead(200, { 'content-type': 'text/html' });
-        response.write(data);
-        response.end();
-      });
   }
+  ///////POST////////////
+
 });
 
 server.listen(PORT, () => {
