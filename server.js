a2f7ae8f1954be53782f6742724d0ba9571678e6
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-23 15:44:36 -1000

    A bit closer

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
 
+
 //404 Response
 function send404(response) {
   response.writeHead(404, { 'Content-Type': 'text/plain' });
@@ -10,28 +11,50 @@ function send404(response) {
 }
 
 const server = http.createServer((request, response) => {
+  let method = request.method;
   let url = request.url;
-  switch (meth) {
-    case ('GET'):
-      fileFetch(url, response);
-      break;
-    case ('POST'):
-      if (url !== 'public/elements') {
-        response.writeHead(400, 'Bad Request', { 'content-type': 'text/plain' });
-        response.end();
-        break;
-      }
 
-      let requestBod = " ";
-
-      request.on('data', function (chunk) {
-        console.log(chunk)
-        requestBod += chunk;
-      })
+  if (request.url.indexOf('public') < 1) { //Add public to url
+    url = 'public' + request.url;
   }
 
-}) 
-    server.listen(PORT, () => {
-      console.log(`server listening on port: ${PORT}`)
+  console.log('Request for ' + url + ' received.');
+  console.log('Method: ', method);
+
+  switch (method) {
+    case ('GET'):
+      fs.readFile('./public/index.html', 'utf-8', (err, data) => {
+        if (err) {
+          fs.readFile('./public/404.html', 'utf-8', (err, data) => {
+            console.log('!!!!!!!', data);
+            response.writeHead(404, { 'content-type': 'text/html' });
+            response.write(data);
+            response.end();
+          })
+        } else {
+          response.writeHead(200, { 'content-type': 'text/html' });
+          response.write(data);
+          response.end();
+        }
+      });
+  //     break;
+  //   case ('GET'):
+  //     fs.readFile('./public/css/styles.css', 'utf-8', (err, data) => {
+  //       response.setHeader('Content-Type', 'text/css')
+  //       response.writeHead(200, 'OK');
+  //       response.write(data);
+  //       response.end();
+  //     });
+  //     break;
+  //   case ('GET'):
+  //   fs.readFile('./public/hydrogen.html', 'utf-8', (err, data) => {
+  //     response.writeHead(200, { 'content-type': 'text/css' });
+  //     response.write(data);
+  //     response.end();
+    // });
+  }
+});
+server.listen(PORT, () => {
+  console.log(`server listening on port: ${PORT}`)
 });
 
