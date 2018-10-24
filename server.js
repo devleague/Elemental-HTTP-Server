b452f0555ff5f97a2a64fc0e76220f1b092b025a
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-24 12:19:52 -1000

    get handler complete

--- server.js
+++ server.js
@@ -1,38 +1,54 @@
+console.log('DRIP TO HARD');
 const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
+const qs = require('querystring')
 
+function getType (url) {
+  let uriArr = url.split('.');
+  // console.log(type.length)
+  let index = uriArr.length-1; //jus a num
+  let type = uriArr[index]; //html 
+  return type
+};
 
+function getHandler(request, response) {
+  let req = request.url; // '/'
+  let met = response.method;
+  req = (req === '/') ? '/index.html': req; //terniary
+  let type = getType(req);
+  fs.readFile('./public' + req, (err, data) => {
+    if (err) {
+      fs.readFile('./public/404.html', 'utf8', (err, data) => {
+        response.setHeader ('Content-Type', 'text/html')
+        response.writeHead(404, 'Bad Request')
+        response.write(data);
+        response.end();
+      })
+    } 
+    else {
+      response.setHeader ('Content-Type', `text/${type}`)
+      response.writeHead (200, 'Request Complete',)
+      response.write(data);
+      response.end();
+    }
+  });
+}
+
+function postHandler () {
+  
+}
 const server = http.createServer((request, response) => {
   let method = request.method;
   let url = request.url;
-
-  if (url.indexOf('public') < 1) { //Add public to url
-    url = 'public' + request.url;
+  
+  // console.log('KEYYYYY', url);
+  // console.log('Method: ', method);
+  ///////////////GET////////
+  switch (request.method) {
+    case 'GET':
+    getHandler(request, response)
   }
-
-  console.log(url);
-  console.log('Method: ', method);
-
-  switch (url) {
-    case 'public/':
-      fs.readFile('public/index.html', (err, data) => {
-        let data2 = data.toString();
-        response.writeHead(200, { 'Content-Type': 'text/html' })
-        response.write(data2)
-        response.end();
-      });
-      break;
-    case `public${request.url}`:
-      fs.readFile(url, (err, data) => {
-        response.writeHead(200, { 'Content-Type': 'text/html' })
-        response.write(data)
-        response.end();
-      })
-      break;
-  }
-  ///////POST////////////
-
 });
 
 server.listen(PORT, () => {
