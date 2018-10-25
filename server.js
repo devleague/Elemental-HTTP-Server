e6818959e68bed2be97fe2c7214f06b18651f419
Shadrach Lagrimas <shadrachlagrimas30@gmail.com>
2018-10-24 19:20:06 -1000

    right before i try

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-console.log('IM GOING CRAZY...');
+console.log('Pray For Me');
 const http = require('http');
 const fs = require('fs');
 const PORT = process.env.PORT || 8080;
@@ -6,24 +6,22 @@ const qs = require('querystring')
 
 function getType(url) {
   let uriArr = url.split('.');
-  // console.log(type.length)
-  let index = uriArr.length - 1; //jus a num
-  let type = uriArr[index]; //html 
+  let index = uriArr.length - 1;
+  let type = uriArr[index];
   return type
 };
-
 function getHandler(request, response) {
-  let req = request.url; // '/'
+  let req = request.url;
   let met = response.method;
-  req = (req === '/') ? '/index.html' : req; //terniary
+  req = (req === '/') ? '/index.html' : req;
   let type = getType(req);
   fs.readFile('./public' + req, (err, data) => {
     if (err) {
       fs.readFile('./public/404.html', 'utf8', (err, data) => {
         response.setHeader('Content-Type', 'text/html')
         response.writeHead(404, 'Bad Request')
-        response.write(data); 
-        response.end(); 
+        response.write(data);
+        response.end();
       })
     }
     else {
@@ -38,8 +36,8 @@ function getHandler(request, response) {
 function postHandler(request, response) {
 
   request.on('data', (chunk) => {
- let body = qs.parse(`${chunk}`); //BIG OBJ, NAME AND STUFF
- let datas = `<!DOCTYPE html>
+    let body = qs.parse(`${chunk}`);
+    let datas = `<!DOCTYPE html>
  <html lang="en">
  <head>
    <meta charset="UTF-8">
@@ -54,18 +52,47 @@ function postHandler(request, response) {
    <p><a href="/">back</a></p>
  </body>
  </html>`;
- console.log(datas)
- fs.writeFile('./public/' + `${body.elementName}.html`, datas, (err) => {
-   console.log('OIOIOIOIOIOIOIOI')
-  if (err) {
-   response.end('404 not found');
+    // console.log(datas)
+    fs.writeFile('./public/' + `${body.elementName}.html`, datas, (err) => {
+     if (err) {
+    response.end('404 not found');
   } else {
-    response.writeHead(200, { 'Content-Type': 'application/json'});
-    response.end(JSON.stringify({ "success": 'true' }));
-  }
- 
-  
-})
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify({ "success": 'true' }));
+
+        fs.readdir('./public', 'utf8', (err, files) => {
+          let lastFile = files[files.length - 1]
+          console.log('this is', lastFile)
+          let ul = '<ul>'
+          if (err) throw err;
+          console.log('KAKAKAKAKA', files);
+          let newIndex = `<!DOCTYPE html>
+          <html lang="en">
+          <head>
+            <meta charset="UTF-8">
+            <title>The Elements</title>
+            <link rel="stylesheet" href="/css/styles.css">
+          </head>
+          <body>
+            <h1>The Elements</h1>
+            <h2>These are all the known elements.</h2>
+            <h3>These are 2</h3>
+            <ol>
+              <li>
+                <a href="/hydrogen.html">Hydrogen</a>
+              </li>
+              <li>
+                <a href="/helium.html">Helium</a>
+                </li>  
+               <li>
+               <a href = "/${lastFile}">${lastFile}</a>
+                </ol>
+                </body>
+                </html>`
+                console.log(newIndex)
+        })
+      }
+    })
   })
 }
 
@@ -73,19 +100,18 @@ const server = http.createServer((request, response) => {
   let method = request.method;
   let url = request.url;
 
-  // console.log('KEYYYYY', url);
-  // console.log('Method: ', method);
-  ///////////////GET////////
   switch (request.method) {
     case 'GET':
       getHandler(request, response)
   }
-  ///////////POST////////
-  switch(request.method) {
-  case 'POST':
-  postHandler(request, response);
+
+  switch (request.method) {
+    case 'POST':
+      postHandler(request, response);
   }
 });
+
+
 
 
 server.listen(PORT, () => {
